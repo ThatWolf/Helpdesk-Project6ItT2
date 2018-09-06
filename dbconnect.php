96bc47f3c2d1d04c0dbbdb1c848887bfb8055b96
yente06 <yente.cool@gmail.com>
2018-09-06 16:02:29 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,7 @@
-<php
+<?php
+$dbServerName = "localhost";
+$dbUserName = "root";
+$dbPassWord = "";
+$dbName = "dbname";
 
-
-?>
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassWord, $dbName);
